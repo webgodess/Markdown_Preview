a0795ffdd932e315133d7d3bb072577e5f0b3802
webgodess <sritarbrt@gmail..com>
2023-09-22 14:29:09 +0200

    feat:prettier plugin applied

--- markdown.js
+++ markdown.js
@@ -22,59 +22,49 @@ At the command prompt, type \`nano\`
 I just love **bold text**.
 
 ![Benjamin Bannekat](https://octodex.github.com/images/bannekat.png)  
-`
+`;
 
-
-
-class Editor extends React.Component{
-  constructor(props){
+class Editor extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      value: init
-    }
-  this.handleChange = this.handleChange.bind(this); 
-    
+      value: init,
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
-  
-  handleChange(e){
+
+  handleChange(e) {
     this.setState({
-      value:e.target.value
-    })
+      value: e.target.value,
+    });
   }
-  
+
   render() {
     return (
-      
-<div>
-       <textarea id="editor" value={this.state.value}  onChange={this.handleChange}/>
-  
-  <Parsed content={this.state.value}/>
-  </div>
-    )
-    
+      <div>
+        <textarea
+          id="editor"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+
+        <Parsed content={this.state.value} />
+      </div>
+    );
   }
-  
 }
 
-
-
-class Parsed extends React.Component{
-  constructor(props){
+class Parsed extends React.Component {
+  constructor(props) {
     super(props);
-    
   }
-  render(){
-    return(
-    <div id="preview">
-       <ReactMarkdown children={this.props.content}/>
-    </div>
-    )
+  render() {
+    return (
+      <div id="preview">
+        <ReactMarkdown children={this.props.content} />
+      </div>
+    );
   }
 }
 
-
-
-
-
-ReactDOM.render(<Editor />, document.getElementById("root"))
-
+ReactDOM.render(<Editor />, document.getElementById("root"));
